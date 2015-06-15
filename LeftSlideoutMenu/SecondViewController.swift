6601b807a62af1ea66f9ecd073f8afaee3abb983
Robert Chen <robert.chen@thorntechnologies.com>
2015-06-15 17:45:55 -0400

    Initial Commit

---
+++ LeftSlideoutMenu/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LeftSlideoutMenu
+//
+//  Created by Robert Chen on 6/15/15.
+//  Copyright (c) 2015 Thorn Technologies. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LeftSlideoutMenuTests/LeftSlideoutMenuTests.swift
@@ -0,0 +1,36 @@
+//
+//  LeftSlideoutMenuTests.swift
+//  LeftSlideoutMenuTests
+//
+//  Created by Robert Chen on 6/15/15.
+//  Copyright (c) 2015 Thorn Technologies. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class LeftSlideoutMenuTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
