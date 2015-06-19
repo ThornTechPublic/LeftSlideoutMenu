bb59178fa8f223864726f74fe05d6ae586bad899
Robert Chen <robert.chen@thorntechnologies.com>
2015-06-19 12:02:28 -0400

    Closes the menu when the device rotates

--- LeftSlideoutMenu/ContainerVC.swift
+++ LeftSlideoutMenu/ContainerVC.swift
@@ -18,6 +18,8 @@ class ContainerVC : UIViewController {
         // Tab bar controller's child pages have a top-left button toggles the menu
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "toggleMenu", name: "toggleMenu", object: nil)
         
+        // Close the menu when the device rotates
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "rotated", name: UIDeviceOrientationDidChangeNotification, object: nil)
     }
     
     // Cleanup notifications added in viewDidLoad
@@ -37,6 +39,18 @@ class ContainerVC : UIViewController {
     // Open is the natural state of the menu because of how the storyboard is setup.
     func openMenu(){
         scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+    }
+    
+    // see http://stackoverflow.com/questions/25666269/ios8-swift-how-to-detect-orientation-change
+    // close the menu when rotating to landscape.
+    // Note: you have to put this on the main queue in order for it to work
+    func rotated(){
+        if UIDeviceOrientationIsLandscape(UIDevice.currentDevice().orientation) {
+            dispatch_async(dispatch_get_main_queue()) {
+                println("closing menu on rotate")
+                self.closeMenu()
+            }
+        }
     }
     
 }
