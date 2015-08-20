a131ece6dfc77f651ac96fec88729f18f774ca2b
Robert Chen <robert.chen@thorntechnologies.com>
2015-08-20 09:22:30 -0400

    Revert "Setting the storyboard constraints such that the left menu is 260 pixels off-screen to the left.  Basically the normal state, as one would expect.  This prevents situations where the menu flies open when switching tabs or rotating the device.  This changes the x-offset numbers, so the logic in ContainerVC had to be updated accordingly."
    This reverts commit 9d05fa1c0c069f42fbb99326951c7d426ba761f6.

--- LeftSlideoutMenu/ContainerVC.swift
+++ LeftSlideoutMenu/ContainerVC.swift
@@ -18,6 +18,9 @@ class ContainerVC : UIViewController {
         // Tab bar controller's child pages have a top-left button toggles the menu
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "toggleMenu", name: "toggleMenu", object: nil)
         
+        // Close the menu when the device rotates
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "rotated", name: UIDeviceOrientationDidChangeNotification, object: nil)
+        
         // LeftMenu sends openModalWindow
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "openModalWindow", name: "openModalWindow", object: nil)
 
@@ -33,18 +36,30 @@ class ContainerVC : UIViewController {
     }
     
     func toggleMenu(){
-        scrollView.contentOffset.x == 0  ? openMenu() : closeMenu()
+        scrollView.contentOffset.x == 0  ? closeMenu() : openMenu()
     }
     
     // Use scrollview content offset-x to slide the menu.
     func closeMenu(animated:Bool = true){
-        scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: animated)
+        scrollView.setContentOffset(CGPoint(x: leftMenuWidth, y: 0), animated: animated)
     }
     
     // Open is the natural state of the menu because of how the storyboard is setup.
     func openMenu(){
         println("opening menu")
-        scrollView.setContentOffset(CGPoint(x: -leftMenuWidth, y: 0), animated: true)
+        scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+    }
+    
+    // see http://stackoverflow.com/questions/25666269/ios8-swift-how-to-detect-orientation-change
+    // close the menu when rotating to landscape.
+    // Note: you have to put this on the main queue in order for it to work
+    func rotated(){
+        if UIDeviceOrientationIsLandscape(UIDevice.currentDevice().orientation) {
+            dispatch_async(dispatch_get_main_queue()) {
+                println("closing menu on rotate")
+                self.closeMenu()
+            }
+        }
     }
     
 }
