e5e2912e091e176f4a0600dd9b035e6c00033b2c
Robert Chen <robert.chen@thorntechnologies.com>
2016-01-21 12:04:39 -0500

    Wiring up the modal mask from the Tab Bar Controller, rather than from the individual tabs. It's important to note that the Presentation has to be set to "Over Current Context". While it's ideal to set "Over Current Context" on the segue, this option is not always available for some reason.  So, instead, you have to set it on the ModalMask View Controller's Attributes Inspector.  The TabVCTemplate now asks the tab bar controller to perform the segue.  Also, there's no more need for the prepareForSegue method, which wasn't able to set the modalPresentationStyle successfully anyway.

--- LeftSlideoutMenu/TabVCTemplate.swift
+++ LeftSlideoutMenu/TabVCTemplate.swift
@@ -20,21 +20,6 @@ class TabVCTemplate : UIViewController {
         
     }
     
-    // The Storyboard let's you set the Modal segue's Presentation to `Current Context`.
-    // However, it doesn't expose the option we want, which is `Over Current Context`.
-    // For iOS 8:
-    // 1. Make sure the segue Presentation is set to `Default`
-    // 2. Set the modalPresentationStyle to OverCurrentContext during prepareForSegue
-    // Note: iOS 7 requires a different approach.
-    //
-    // see: http://stackoverflow.com/questions/26032472/presenting-semi-transparent-viewcontroller-that-works-both-in-ios7-and-ios8
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "OpenModalMask" {
-            let modalMask = segue.destinationViewController as! ModalMask
-            modalMask.modalPresentationStyle = .OverCurrentContext
-        }
-    }
-    
     deinit {
         NSNotificationCenter.defaultCenter().removeObserver(self)
     }
@@ -48,7 +33,7 @@ class TabVCTemplate : UIViewController {
     @IBAction func toggleMenu(sender: AnyObject) {
         NSNotificationCenter.defaultCenter().postNotificationName("toggleMenu", object: nil)
         // open modal mask
-        performSegueWithIdentifier("OpenModalMask", sender: nil)
+        tabBarController?.performSegueWithIdentifier("OpenModalMask", sender: nil)
     }
     
 }
